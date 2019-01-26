acb5cfd5529da2b07ca37f369f079ad7e84869b2
DIVYANSH TRIPATHI <divyanshtripathi1998@gmail.com>
2019-01-26 23:03:02 +0530

    pehla adhyay

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(logger("dev"));
+
+var gyaansagar = require("./gyaansagar.js");
+
+app.get("/search/:term", (req, res) => {
+  var term = req.params.term;
+  var result = gyaansagar.map(shabd => {
+    if (shabd.toLowerCase().includes(term)) return shabd;
+  });
+  res.json(result);
+});
+
+app.listen("5000", () => {
+  console.log("Listeneing at port 5000");
+});
